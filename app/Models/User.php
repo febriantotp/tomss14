2f36100f9da39d3c40354b33a8c9f8c9d46590a2
febriantotp <163731710+febriantotp@users.noreply.github.com>
2025-12-22 04:08:28 +0700

    Implement FilamentUser interface in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -39,7 +42,7 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
-    public function canAccessPanel(\Filament\Panel $panel): bool
+    public function canAccessPanel(Panel $panel): bool
     {
         // Untuk tes, izinkan semua user
         return true;
