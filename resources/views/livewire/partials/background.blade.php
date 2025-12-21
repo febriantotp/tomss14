6e1ac5631ef05586ab5decb0f48d75bf69e369ab
febriantotp <163731710+febriantotp@users.noreply.github.com>
2025-12-22 03:10:02 +0700

    Enable video background in background.blade.php
    Uncomment video background code in the Blade template.

--- resources/views/livewire/partials/background.blade.php
+++ resources/views/livewire/partials/background.blade.php
@@ -1,6 +1,6 @@
 <div class="video-background bg-slate-800">
-    {{-- <video autoplay muted loop>
+    <video autoplay muted loop>
         <source src="{{ asset('storage/backgrounds/bgvideo.mp4') }}" type="video/mp4">
         Your browser does not support the video tag.
-    </video> --}}
+    </video>
 </div>
