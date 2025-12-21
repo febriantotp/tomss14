1a03cdc9315b39d32443e8d85c3c7b9de7823ae2
febriantotp <163731710+febriantotp@users.noreply.github.com>
2025-12-22 04:37:39 +0700

    Enhance AppServiceProvider with Filament and storage link
    Added Filament panel registration and ensured storage link creation in production environment.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Artisan;
+use Filament\Facades\Filament;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,14 +21,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Daftarkan panel dari bootstrap/providers.php
         Filament::registerPanels(require base_path('bootstrap/providers.php'));
 
+        // Pastikan storage link otomatis di production
         if (app()->environment('production')) {
-        $publicStorage = public_path('storage');
-        if (!is_link($publicStorage)) {
-            Artisan::call('storage:link');
+            $publicStorage = public_path('storage');
+            if (!is_link($publicStorage)) {
+                Artisan::call('storage:link');
+            }
         }
-    }
-
     }
 }
