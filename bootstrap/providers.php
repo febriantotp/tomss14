1c665b30650c324c7dc4bfcb4df94e9f82f3ecfb
febriantotp <163731710+febriantotp@users.noreply.github.com>
2025-12-22 04:30:27 +0700

    Add Supertomss14PanelProvider to providers list

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Providers\Filament\Supertomss14PanelProvider;
+
+
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\Supertomss14PanelProvider::class,
