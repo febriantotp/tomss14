abd5b422d43bcd2795f9d1c1d33ea5c16e54f74b
febriantotp <febriantotripratomo14@gmail.com>
2025-12-23 03:12:23 +0700

    ai anjing 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Artisan;
 use Filament\Facades\Filament;
+use Illuminate\Support\Facades\Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,8 +22,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Log::info('Entering Supertomss14PanelProvider::panel');
         // Daftarkan panel dari bootstrap/providers.php
+        Log::info('AppServiceProvider boot start');
+
         Filament::registerPanels(require base_path('bootstrap/providers.php'));
+        Log::info('AppServiceProvider boot end');
+
+        
+
 
         // Pastikan storage link otomatis di production
         if (app()->environment('production')) {
